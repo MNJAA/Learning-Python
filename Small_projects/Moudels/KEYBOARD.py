f2e787e030984b9e4704689d8cef19bd3ae1144e
MNJAA <hlmm8645@gmail.com>
2024-02-08 11:24:48 +0300

    idle killer

---
+++ Small_projects/Moudels/KEYBOARD.py
@@ -0,0 +1,21 @@
+import keyboard
+from time import sleep,time
+
+# 5 seconds to enter the game
+sleep(5)
+
+# this mkae your charchter moves to kill the idling 
+while True:
+    #time() = current time
+    Then = time()
+    sleep(60)
+    Now = time()
+    if Now - Then > 60:
+        for _ in range(3):
+            keyboard.press('a')
+            sleep(1)
+            keyboard.press('d')
+            sleep(1)
+            keyboard.press('w')
+            sleep(1)
+            keyboard.press('s')
